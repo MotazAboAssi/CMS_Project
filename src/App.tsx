8fd6998bf92f9c36bb6470093602a45511066ccc
MotazAboAssi <motazaboassi@gmail.com>
2026-05-04 15:22:05 +0300

    active shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "./components/ui/button"
 
-export default function App() {
-  return <div></div>;
+function App() {
+  return (
+    <Card className="max-w-sm">
+      <CardHeader>
+        <CardTitle>Project Overview</CardTitle>
+        <CardDescription>
+          Track progress and recent activity for your Vite app.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        Your design system is ready. Start building your next component.
+      </CardContent>
+      <Button variant={"outline"}>Click me</Button>
+    </Card>
+  )
 }
+
+export default App
